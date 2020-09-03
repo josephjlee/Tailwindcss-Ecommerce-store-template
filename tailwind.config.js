383fc5008beb6a5363d224d3ee522f37d7797ee4
raju-ahammad <web.rajuahammad@gmail.com>
2020-09-03 22:35:35 +0600

    create navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   purge: [],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      'xxl': '1590',
+    },
     extend: {},
   },
   variants: {},
