b9c4317a6c69312ca12654eed36a7b7a2211f056
raju-ahammad <web.rajuahammad@gmail.com>
2020-09-05 00:24:23 +0600

    change toggle loop

--- code/js/app.js
+++ code/js/app.js
@@ -1,9 +1,16 @@
 const triggers = document.querySelectorAll(".menu-trigger")
 console.log(triggers);
 
-for(let i = 0; i < triggers.length; i++) {
-    triggers[i].addEventListener("click", function(e){
+triggers.forEach(trigger => {
+    trigger.addEventListener('click', function(e) {
         e.preventDefault();
         this.classList.toggle("is-open")
     })
-}
+})
+
+// for(let i = 0; i < triggers.length; i++) {
+//     triggers[i].addEventListener("click", function(e){
+//         e.preventDefault();
+//         this.classList.toggle("is-open")
+//     })
+// }
